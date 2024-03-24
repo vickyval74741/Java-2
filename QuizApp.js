e4d3ea91e072ad5ff4256912c173bab661600280
vickyval74741 <vickyval002@gmail.com>
2024-03-23 17:23:51 -0700

    quiz

--- QuizApp.js
+++ QuizApp.js
@@ -3,6 +3,7 @@
 
 let username = ((prompt("Enter your Username") || "").trim()).toUpperCase();
 
+// Keep asking the user for their username until it is provided
 while (!username) {
     console.log("You entered no username")
     alert("You entered no username.\nPlease enter your username.")
@@ -96,31 +97,56 @@ if (username) {
 
 }
 
+// Define questions and answers
+const questionsAndAnswers = [
+    { question: "What is the birth year of Dr. Nnamdi Azikiwe?", answer: 1904 },
+    { question: "What is the birth year of Gen. Yakubu Gowon?", answer: 1934 },
+    { question: "What is the birth year of Gen. Olusegun Obasanjo?", answer: 1937 },
+    { question: "What is the birth year of Alhaji Shehu Shagari?", answer: 1925 },
+    { question: "What is the birth year of Gen. Muhammadu Buhari?", answer: 1942 },
+    { question: "What is the birth year of Chief Ernest Shonekan", answer: 1936 },
+    { question: "What is the birth year of Gen. Sani Abacha?", answer: 1943 },
+    { question: "What is the birth year of Alhaji Umaru Yar'Adua?", answer: 1951 },
+    { question: "What is the birth year of Dr. Goodluck Jonathan?", answer: 1957 },
+    { question: "What is the birth year of Chief Bola Ahmed Tinubu?", answer: 1952 }
+];
 
-const testBtn = document.getElementById("testBtn");
-const firstInputResult = document.getElementById("firstInputResult");
+// This is to reference the "Start Quiz" button on the document
+const quizButton = document.getElementById("btnQuiz");
 
+// This is to add an Event listener to the "Start Quiz" button, so that when the button is clicked the underlisted function will be performed
+quizButton.addEventListener("click", () => {
 
-testBtn.addEventListener("click", () => {
-    const inputValue = prompt("Enter any value (text or number)");
+    // This initializes the "total score" to 0 
+    let totalScore = 0;
 
-    const testValue =parseInt (inputValue);
-    
+        // This initializes the "wrong Answers" to an empty variable
+        let wrongAnswers = [];
 
+        // Loop through each question
+        questionsAndAnswers.forEach((item, index) => {
+            let userResponse = parseInt(prompt(item.question).trim());
+            if (!userResponse || userResponse !== item.answer) {
+                
+                wrongAnswers.push({ question: item.question, correctAnswer: item.answer, userResponse });
 
-    if (isNaN(testValue)){
+            } else {
+                totalScore += 10;
+            }
+        });
 
-        firstInputResult.innerHTML = `The Data you entered is "<span style = "font-weight: bold;">${inputValue}"</span>.\n <br>Your Data Type is <b> String.</b>`;
-    }else{
+        // This is to reference the "display result" element on the document
+        const displayResult = document.getElementById("displayResult");
+        displayResult.innerHTML = `Your final score is <span style="font-size: 24px; font-weight: bold;">${totalScore}% out of 100 </span>`;
 
-        firstInputResult.innerHTML = `The Data you entered is "<span style = "font-weight: bold;">${inputValue}"</span>.\n <br>Your Data Type is <b> Number.</b>`;
-    }
+        // This is to reference the "display wrong answer" element on the document
+        const displayWrongAnswer = document.getElementById("displayWrongAnswer");
+        wrongAnswers.forEach((item, index) => {
+            let listItem = document.createElement("li");
+            listItem.innerHTML = `Question: ${item.question},  <br> Correct Answer: ${item.correctAnswer}, <br> Your Answer: ${item.userResponse}`;
+            displayWrongAnswer.appendChild(listItem);
 
+            const listHeading = document.getElementById("listHeading");
+            listHeading.innerHTML = "List of Incorrect answered Question";
+        });
 });
-
-
-
-
-
-
-
